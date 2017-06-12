35c4bd5ed2346df56bda1563fe23eedc72becb9c
Merrill <merrill@MerrilldeMacBook-Pro.local>
2017-06-12 17:43:40 +0800

    Initial Commit

---
+++ TANCalendar/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TANCalendar
+//
+//  Created by merrill on 2017/6/12.
+//  Copyright © 2017年 merrill. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TANCalendar/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TANCalendar
+//
+//  Created by merrill on 2017/6/12.
+//  Copyright © 2017年 merrill. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
